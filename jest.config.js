00a96856faedae2cd0b3877fba54a3acf57eb473
Jon Jaques <jaquers@gmail.com>
2018-12-21 19:11:22 -0600

    fixup scripts

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,18 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-};
+  projects: [
+    {
+      roots: ["packages/serve"],
+      preset: 'ts-jest',
+      testEnvironment: 'node',
+      bail: true,
+      verbose: true,
+    },
+    {
+      roots: ["packages/web"],
+      preset: 'ts-jest',
+      testEnvironment: 'node',
+      bail: true,
+      verbose: true,
+    }
+  ],
+}
