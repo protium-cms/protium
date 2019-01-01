2cb05b5782b47902fe332e800f6c65b3f3f39bc2
Jon Jaques <jaquers@gmail.com>
2019-01-01 12:00:31 -0600

    Add body parser and compression to web, api

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,6 +1,11 @@
+import bodyParser from 'body-parser'
+import compression from 'compression'
 import Express from 'express'
 
 export const api = Express()
+
+api.use(compression())
+api.use(bodyParser.json({strict: true}))
 
 api.get('/', (req, res) => {
   res.json({hello: 'world'})
--- packages/web/src/index.ts
+++ packages/web/src/index.ts
@@ -1,4 +1,6 @@
 import {getContext} from '@protium/assets/lib/utils'
+import bodyParser from 'body-parser'
+import compression from 'compression'
 import Express from 'express'
 import Path from 'path'
 
@@ -8,6 +10,8 @@ const assetContext = getContext('@protium/assets')
 if (!assetContext) {
   throw new Error(`Can't find asset module`)
 }
+app.use(compression())
+app.use(bodyParser.json({strict: true}))
 
 app.use('/assets', Express.static(Path.resolve(assetContext, 'lib'), {
   fallthrough: true,
