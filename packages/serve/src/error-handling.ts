f5d0b53b2f8efe5aaa56c3a2254a070832166d32
Jon Jaques <jaquers@gmail.com>
2019-01-25 17:25:00 -0600

    Exit immediately?

--- packages/serve/src/error-handling.ts
+++ packages/serve/src/error-handling.ts
@@ -4,15 +4,15 @@ process
   .on('uncaughtException', (err) => {
     logger.warn('Unhandled exception detected: exiting to prevent bad state...')
     logger.error(err)
-    process.nextTick(() => process.exit(1))
+    process.exit(1)
   })
   .on('unhandledRejection', (reason, promise) => {
     logger.warn('Unhandled promise rejection detected: exiting to prevent bad state...')
     logger.error(reason)
-    process.nextTick(() => process.exit(1))
+    process.exit(1)
   })
   .on('multipleResolves', (type, promise, reason) => {
     logger.warn('Multiple promise resolutions detected: exiting to prevent bad state...')
     logger.error(`${type}(${JSON.stringify(reason)})`)
-    process.nextTick(() => process.exit(1))
+    process.exit(1)
   })
